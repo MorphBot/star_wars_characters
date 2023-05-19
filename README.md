bad5000453a69ac67dd21258f27b5e9b026fd865
Martin Khúla <martinkhla@gmail.com>
2023-05-19 14:35:51 +0200

    Add quasar and SW chars search

---
+++ README.md
@@ -0,0 +1,14 @@
+<img src="https://img.shields.io/npm/v/quasar-ui-star-wars-characters.svg?label=quasar-ui-star-wars-characters">
+
+
+Compatible with Quasar UI v1 and Vue 2.
+
+# Structure
+* [/ui](ui) - standalone npm package
+
+
+# Donate
+If you appreciate the work that went into this project, please consider [donating to Quasar](https://donate.quasar.dev).
+
+# License
+MIT (c) 
---
+++ ui/dev/src/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page padding class="row justify-center">
+    <q-list dense class="list">
+      <div class="text-h4 q-mb-md">Test pages</div>
+      <q-item
+        v-for="page in pages"
+        :key="page.path"
+        :to="page.path"
+      >
+        <q-item-section avatar>
+          <q-icon name="pages" />
+        </q-item-section>
+        <q-item-section>
+          {{ page.title }}
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+import pages from '../router/pages'
+
+export default {
+  created () {
+    this.pages = pages
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list {
+  width: 700px;
+  max-width: 100%;
+}
+
+.fade-enter-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
